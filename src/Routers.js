8126840f05d4e0342661c84f6ce92c7203eea364
zhouxishun <1050494995@qq.com>
2019-09-26 19:29:46 +0800

    css引入

--- src/Routers.js
+++ src/Routers.js
@@ -1,17 +1,5 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-
 import Home from '@/page/Home';
 import Login from '@/page/Login';
-
-// export default () => {
-//   return (
-//     <div>
-//       <Route path="/" component={Home} exact />
-//       <Route path="/login" component={Login} />
-//     </div>
-//   );
-// };
 
 const router = [
   {
